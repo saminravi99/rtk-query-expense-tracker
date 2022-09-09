fc6540ad102d5b644e286d4a893acc16fbc28ec7
saminravi99 <saminisrar1@gmail.com>
2022-09-09 17:09:49 +0600

    base url changed

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:9000",
+  baseURL:
+    "https://redux-toolkit-expense-tracker-saminravi99.onrender.com/transactions",
 });
 
 export default axiosInstance;
+
+//http://localhost:9000
+//https://redux-toolkit-expense-tracker-saminravi99.onrender.com/transactions
